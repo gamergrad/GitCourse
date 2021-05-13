b07ac669a04aa1b127c994d4f481bb6bb4dac688
Alexey Tereshchuk <vt53@yandex.ru>
2021-05-13 18:23:01 +0700

    Add all files to the directory

--- forTest.cpp
+++ forTest.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <vector>
+#include "header.h"
 
 int main(){
 std::cout << "Hello world" << std::endl;
 
 std::vector<int> rand_ints;
+
+sektor j;
+
+j.begin = 10;
+j.end = 20;
 
 for (size_t i = 0; i < 300; i++)
 {
@@ -16,6 +22,7 @@ for (auto i : rand_ints)
     std::cout << i << std::endl;
 }
 
+std::cout << j.begin <<'\t'<< j.end;
 
 return 0;
 }
---
+++ header.h
@@ -0,0 +1,5 @@
+#pragma once
+
+struct sektor {
+    int begin, end;
+};
