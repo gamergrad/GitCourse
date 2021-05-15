c12b60d119d71ee2c9d915c72bf1de5f26ca7bbe
Alexey Tereshchuk <vt53@yandex.ru>
2021-05-15 18:40:22 +0700

    Add fun in Header TEST CHECKOUT with files

--- header.h
+++ header.h
@@ -5,3 +5,10 @@
 struct sektor {
     int begin, end;
 };
+
+void foo(const std::string& str, unsigned int n ){
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cout << str <<std::endl;
+    } 
+}
