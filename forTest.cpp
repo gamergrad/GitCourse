86919fe43fedd66e72e57540e981ee83b75704d1
Alexey Tereshchuk <vt53@yandex.ru>
2021-05-15 12:51:37 +0700

    Learn diff commit

--- forTest.cpp
+++ forTest.cpp
@@ -8,18 +8,24 @@ std::cout << "Hello world" << std::endl;
 std::vector<int> rand_ints;
 
 sektor j;
+int YES;
 
 j.begin = 10;
 j.end = 20;
 
-for (size_t i = 0; i < 300; i++)
+for (size_t i = 0; i < 30; i++)
 {
     rand_ints.push_back(rand()%100);
+    
+    if(rand_ints[i]%2)
+    std::cout << "==============NO NO NO======================" << std::endl;
 }
 
 for (auto i : rand_ints)
 {
     std::cout << i << std::endl;
+    if(i%2)
+    std::cout << "====================YES YES YES==============" << std::endl;
 }
 
 std::cout << j.begin <<'\t'<< j.end;
